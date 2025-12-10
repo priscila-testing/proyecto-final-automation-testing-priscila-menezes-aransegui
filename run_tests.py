675197393bc4c5b2c59fa3bd62f20a1a43cc3ccc
Priscila Menezes Aransegui <priscilaqatesting>
2025-12-10 20:46:57 -0300

    update integracion continua

--- run_tests.py
+++ run_tests.py
@@ -1,17 +1,3 @@
 import pytest
 
-# Lista de archivos de pruebas a ejecutar
-test_files = [
-    "tests/test_login.py",
-    "tests/test_inventory.py",
-    "tests/test_cart.py",
-    "tests/test_cart_json.py",
-    "tests/test_api_reqres.py"
-]
-
-# Argumentos para ejecutar las pruebas: archivos + reporte HTML
-pytest_args = test_files + ["--html=report.html","--self-contained-html","-v"]
-
-pytest.main(pytest_args)
-
-
+pytest.main("tests/","--html=reports/report.html","--self-conteined-html","-v")
